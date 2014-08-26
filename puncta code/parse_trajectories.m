7056d31af2ad45bb1a02f2458690bafbaec0b101
Matt Valley <valley@gmail.com>
2014-08-26 21:23:41 +0200

    changes to parse by days

--- puncta code/parse_trajectories.m
+++ puncta code/parse_trajectories.m
@@ -1,24 +1,27 @@
 clear all;
 
 rootdirs = {'C:\Users\supersub\Desktop\Data\text files\MC lateral',...
-    'C:\Users\supersub\Desktop\Data\text files\TC lateral'};%...
-    %'C:\Users\supersub\Desktop\Data\text files\10min controls'};
-%days = (1:8); %days to analyze
+    'C:\Users\supersub\Desktop\Data\text files\TC lateral',...
+    'C:\Users\supersub\Desktop\Data\text files\10min controls'};
+days = [1:8]; %days to analyze
 
-plotcolors = {'k', 'r'};
+plotcolors = {'k', 'r', 'b'};
 
-for n = 1:length(rootdirs)
-    [condition(n).allpuncta, roi] = parse_trajectory(rootdirs{n});
-
-    all_lifetimes = [];
-    for k = 1:roi
-        all_lifetimes = cat(2, all_lifetimes, condition(n).allpuncta(k).lifetimes);
-        all_cumhist = condition(n).allpuncta(k).cumhist;
-        plot(all_cumhist(1:end-1), plotcolors{n}); ylim([0 1]); hold on;
-        fitx = 1:(length(all_cumhist)-2);
-        fity = all_cumhist(2:end-1)'; %dont fit to single puncta (non trajectory puncta)
-        fitcoeffs(:,k) = polyfit(fitx, fity, 1);
-        clear all_cumhist;
+for d = 1:size(days,1)
+    figure
+    for n = 1:length(rootdirs)
+        [condition(n).allpuncta, roi] = parse_trajectory(rootdirs{n}, days(d,:));
+        all_lifetimes = [];
+        for k = roi
+            all_lifetimes = cat(2, all_lifetimes, condition(n).allpuncta(k).lifetimes);
+            all_cumhist = condition(n).allpuncta(k).cumhist;
+            plot(all_cumhist(1:end-1), plotcolors{n}); ylim([0 1]); hold on;
+                title(['Lifetime of puncta day ' mat2str(days(d,:))]);
+            fitx = 1:(length(all_cumhist)-2);
+            fity = all_cumhist(2:end-1)'; %dont fit to single puncta (non trajectory puncta)
+            fitcoeffs(:,k) = polyfit(fitx, fity, 1);
+            clear all_cumhist
+        end
     end
 end
 
--- puncta code/parse_trajectory.m
+++ puncta code/parse_trajectory.m
@@ -1,14 +1,15 @@
-%function [allpuncta, roi] = parse_trajectory(rootdir)
-clear all 
+function [allpuncta, roi_valid] = parse_trajectory(rootdir, days_toanalyze)
 
-rootdir = 'C:\Users\supersub\Desktop\Data\text files\MC lateral';
+%clear all 
+
+%rootdir = 'C:\Users\supersub\Desktop\Data\text files\MC lateral';
     
 cd(rootdir);
 FullList = dir;
 MaximaList = dir(['*.txt']);
 roi = length(MaximaList); %number of rois
 
-days_toanalyze = (1:8); % later compare this to total dataset size and skip rois that have remainder < 4 
+%days_toanalyze = (1:8); % [1..8]
 
 for k = 1:roi %for each text file of maxima
     maximaname = MaximaList(k).name;
@@ -37,10 +38,10 @@ for k = 1:roi %for each text file of maxima
     total_particles = total_particles(total_indxs+1);
     total_days = length(total_particles)/2; % detecting the number of days (frames)
     
-    if max(days_toanalyze) > total_days
-        disp 'skipped'
-        continue        
-    end    
+%     if length(days_toanalyze) > total_days
+%         disp 'skipped'
+%         continue        
+%     end    
     
     parens = strmatch('%%', frame);
     traj_idx = parens(1:2:end)+2; %every other "%%" marks trajectory start
@@ -48,25 +49,21 @@ for k = 1:roi %for each text file of maxima
         
     for t = 1:length(traj_idx) %for all trajectories
         firstobs = str2double(frame{traj_idx(t)});
-        if firstobs <= max(days_toanalyze)
-            puncta_interest(t) = 1;
-        else
-            puncta_interest(t) = 0;
-        end
-        %puncta(t).firstobs = firstobs;
-        %puncta(t).interesting = puncta_interest(t); % choose just those trajectories that exist within the days of interest
-    end
-    interest_indx = find(puncta_interest==1);
-    
-    for t = interest_indx % build structure using only trajectories of interest
-        if t == length(interest_indx)
+        %interest_indx = find(puncta_interest==1);
+        if t == length(traj_idx)
             puncta(t).lifetime = length(x) - traj_idx(t);
-            puncta(t).framesobs = str2double(frame(traj_idx(t):end));
+            framesobs = str2double(frame(traj_idx(t):end));
         else
             puncta(t).lifetime = traj_idx(t+1)-3 - traj_idx(t);
-            puncta(t).framesobs = str2double(frame(traj_idx(t):(traj_idx(t+1)-3)));
+            framesobs = str2double(frame(traj_idx(t):(traj_idx(t+1)-3)));
         end
-        %puncta(t).lifetime = traj_idx(t+1)-3 - traj_idx(t);
+        framesobs_offset = framesobs - days_toanalyze(1)+1; %set first frame observation to 1
+        framesobs_offset(framesobs_offset < 0) = []; % if observation before first day, make null
+        framesobs_offset(framesobs_offset > length(days_toanalyze)) = [];
+        if length(framesobs_offset) == 1
+            framesobs_offset = [];
+        end
+        puncta(t).framesobs = framesobs_offset;
     end
     
     allpuncta_intraj = [];
@@ -86,9 +83,11 @@ for k = 1:roi %for each text file of maxima
     %puncta_intraj = mean_ave_len*length(traj_s); % = average traj length * num trajectories
     %num_singles = sum_total_particles - totpuncta_intraj;
         
-    for n = 1:length(puncta)
-        ltime_cumhist(n) = puncta(n).lifetime;
+    for n = 1:length(traj_idx)
+        ltime_cumhist(n) = length(puncta(n).framesobs); % get lifetime
     end
+    %ltime_cumhist(ltime_cumhist > max(days_toanalyze)) = 4; %force liftimes into 4-day format
+    %ltime_cumhist(ltime_cumhist < min(days_toanalyze)) = 1;
 
     %ltime_cumhist = cat(2, ltime_cumhist, zeros(1,num_singles)); %add non-trajectory single particles to cumulative histogram
     cumhist = ecdf(ltime_cumhist, 'function', 'survivor');
@@ -98,16 +97,17 @@ for k = 1:roi %for each text file of maxima
     allpuncta(k).cumhist = cumhist;
     allpuncta(k).lifetimes = ltime_cumhist;
     allpuncta(k).puncta_matrix = pun;
-    allpuncta(k).puncta_interest = puncta_interest;
     
+    roi_valid(k) = k;
+    roi_valid(roi_valid==0) = [];
     clear puncta cumhist lentraj ltime_cumhist frame parens traj_idx puncta_interest interest_indx pun
 end
+% 
+% all_lifetimes = [];
+%     for k = 1:roi
+%         all_lifetimes = cat(2, all_lifetimes, allpuncta(k).lifetimes);
+%         all_cumhist = allpuncta(k).cumhist;
+%         plot(all_cumhist(1:end-1), 'k'); ylim([0 1]); hold on;
+%     end
 
-all_lifetimes = [];
-    for k = 1:roi
-        all_lifetimes = cat(2, all_lifetimes, allpuncta(k).lifetimes);
-        all_cumhist = allpuncta(k).cumhist;
-        plot(all_cumhist(1:end-1), 'k'); ylim([0 1]); hold on;
-    end
-
-%end
+end
