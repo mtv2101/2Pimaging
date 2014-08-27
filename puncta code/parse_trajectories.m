f860f17f28477545da913ab4b1825656d3b40a8f
Matt Valley <valley@gmail.com>
2014-08-27 17:11:11 +0200

    add x/y/z coords parsed by day
    in progress - code still broken

--- puncta code/parse_trajectories.m
+++ puncta code/parse_trajectories.m
@@ -1,7 +1,7 @@
 clear all;
 
-rootdirs = {'C:\Users\supersub\Desktop\Data\text files\MC lateral',...
-    'C:\Users\supersub\Desktop\Data\text files\TC lateral',...
+rootdirs = {'C:\Users\supersub\Desktop\Data\text files\TC lateral enriched',...
+    'C:\Users\supersub\Desktop\Data\text files\TC lateral non-enriched',...
     'C:\Users\supersub\Desktop\Data\text files\10min controls'};
 days = [1:8]; %days to analyze
 
@@ -17,13 +17,21 @@ for d = 1:size(days,1)
             all_cumhist = condition(n).allpuncta(k).cumhist;
             plot(all_cumhist(1:end-1), plotcolors{n}); ylim([0 1]); hold on;
                 title(['Lifetime of puncta day ' mat2str(days(d,:))]);
-            fitx = 1:(length(all_cumhist)-2);
+            fitx = 1:(size(all_cumhist,1)-2);
             fity = all_cumhist(2:end-1)'; %dont fit to single puncta (non trajectory puncta)
-            fitcoeffs(:,k) = polyfit(fitx, fity, 1);
+            condition(n).allpuncta(k).fitcoeffs = polyfit(fitx, fity, 1);
             clear all_cumhist
         end
+    condition(n).length = pathlengths(condition(n).allpuncta);
+    condition(n).all_lifetimes = all_lifetimes;
+    clear all_lifetimes
     end
 end
 
-%[cumhist xaxis upconfidence downconfidence] = ecdf(all_lifetimes, 'bounds', 'on');
-%output = [cumhist, xaxis, upconfidence, downconfidence];
+figure;
+for n = 1:length(rootdirs)
+    h = cdfplot(condition(n).length); hold on;    
+    %h = get(gca, 'children');
+    set(h,'Color',plotcolors{n});   
+    xlim([0 10]);    
+end
--- puncta code/pathlengths.m
+++ puncta code/pathlengths.m
@@ -1,12 +1,14 @@
+function [length] = pathlengths(allpuncta)
+
 j=1;
 for n = 1:size(allpuncta,2)
     for i = 1:size(allpuncta(n).puncta,2)
-        for z = 2:size(allpuncta(n).puncta(i).x,2)
-            xtrajstart = allpuncta(n).puncta(i).x(z-1);
-            xtrajend = allpuncta(n).puncta(i).x(z);
+        for k = 2:size(allpuncta(n).puncta(i).x,2)
+            xtrajstart = allpuncta(n).puncta(i).x(k-1);
+            xtrajend = allpuncta(n).puncta(i).x(k);
             lenx = xtrajstart-xtrajend;
-            ytrajstart = allpuncta(n).puncta(i).y(z-1);
-            ytrajend = allpuncta(n).puncta(i).y(z);
+            ytrajstart = allpuncta(n).puncta(i).y(k-1);
+            ytrajend = allpuncta(n).puncta(i).y(k);
             leny = ytrajstart-ytrajend;
             length(j) = sqrt((lenx^2)+(leny^2));
             j = j+1;
@@ -14,4 +16,6 @@ for n = 1:size(allpuncta,2)
     end
 end
 
-ecdf(length);
+%ecdf(length, 'bounds', 'on');
+
+end
