66b0151f904130546f38a55088cfa77b8179e797
Matt Valley <valley@gmail.com>
2014-09-02 20:53:23 +0200

    bug fixes to parse_traj

--- puncta code/parse_trajectory.m
+++ puncta code/parse_trajectory.m
@@ -36,12 +36,8 @@ for k = 1:roi %for each text file of maxima
     %total_indxs = total_indxs(2:(2+length(days))); %these are the "Frame" we want
     total_particles = dataarray{1,2};
     total_particles = total_particles(total_indxs+1);
+    total_particles = str2double(total_particles(1:length(total_particles)/2)); % cut out text data trailing particle data
     total_days = length(total_particles)/2; % detecting the number of days (frames)
-    
-    %     if length(days_toanalyze) > total_days
-    %         disp 'skipped'
-    %         continue
-    %     end
     
     parens = strmatch('%%', frame);
     traj_idx = parens(1:2:end)+2; %every other "%%" marks trajectory start
@@ -49,51 +45,51 @@ for k = 1:roi %for each text file of maxima
     
     for t = 1:length(traj_idx) %for all trajectories
         firstobs = str2double(frame{traj_idx(t)});
-        %interest_indx = find(puncta_interest==1);
         if t == length(traj_idx)
-            puncta(t).lifetime = length(x) - traj_idx(t);
             framesobs = str2double(frame(traj_idx(t):end));
         else
-            puncta(t).lifetime = traj_idx(t+1)-3 - traj_idx(t);
-            framesobs = str2double(frame(traj_idx(t):(traj_idx(t+1)-3)));
+            framesobs = str2double(frame(traj_idx(t):(traj_idx(t+1)-3))); %subtract 3 to index past the two "%%" marks
         end
-        framesobs_offset = framesobs - days_toanalyze(1)+1; %set first frame observation to 1
-        framesobs_offset(framesobs_offset < 0) = []; % if observation before first day, make null
+        framesobs_offset = framesobs - days_toanalyze(1)+2; %shift so first frame eq 1 - add 2 because we call day 1 as 1 even though mosaic calls it day0
+        framesobs_offset(framesobs_offset <= 0) = []; % if observation before first day, make null
         framesobs_offset(framesobs_offset > length(days_toanalyze)) = [];
-        if length(framesobs_offset) == 1
+        if length(framesobs_offset) == 1 || ~isnumeric(framesobs_offset)
             framesobs_offset = [];
         end
         puncta(t).framesobs = framesobs_offset;  
-        [nn, fr_intsect, nm] = intersect(framesobs, days_toanalyze-1); %get index of only days that have trajectories that are in desired date range
-        for n = 1:length(framesobs)            
-            allx(n) = str2double(x{traj_idx(t)+n-1});
-            ally(n) = str2double(y{traj_idx(t)+n-1});
-            allz(n) = str2double(z{traj_idx(t)+n-1});
-        end
-        puncta(t).x = allx(fr_intsect);
-        puncta(t).y = ally(fr_intsect);
-        puncta(t).z = allz(fr_intsect);
-        clear allx ally allz framesobs framesobs_offset framesobs_intersect
     end
+    
+    i=0;
+    for t = 1:length(puncta)
+        if isnumeric(puncta(t).framesobs(1)) %skip trajectories that are set to [];
+            [nn, fr_intsect, nm] = intersect(puncta(t).framesobs, days_toanalyze); %get index of only days that have trajectories that are in desired date range
+            for n = 1:length(puncta(t).framesobs)
+                allx(n) = str2double(x{traj_idx(t)+n-1});
+                ally(n) = str2double(y{traj_idx(t)+n-1});
+                allz(n) = str2double(z{traj_idx(t)+n-1});
+            end
+            puncta(t).x = allx(fr_intsect);
+            puncta(t).y = ally(fr_intsect);
+            puncta(t).z = allz(fr_intsect);            
+            i=i+1;
+            numtrajs = i;
+        end            
+    end    
+    puncta = puncta(1:numtrajs);
     
     allpuncta_intraj = [];
     for d = 1:days_toanalyze(end)-1 %dont iterate to last day which is all single
         for n = 1:length(puncta)
-            pun(d,n) = length(find(puncta(n).framesobs == d)); % get each roi with a puncta detected that day
-            allpuncta_intraj = cat(2, allpuncta_intraj, pun(d,n));
+            pun(d,n) = length(find(puncta(n).framesobs == d)); % get each roi with a puncta detected that day            
         end
+        allpuncta_intraj = cat(2, allpuncta_intraj, pun(d,:));
     end
     
-    sum_total_particles = sum(str2double(total_particles(days_toanalyze)));
+    sum_total_particles = sum(total_particles(days_toanalyze(1:end-1))); % dont get trajectories from last day - single also not counted from last day
     totpuncta_intraj = sum(allpuncta_intraj);
-    %for p = interest_indx
-    %    ave_len(p) = puncta(p).lifetime;
-    %end
-    %mean_ave_len = mean(ave_len+1);
-    %puncta_intraj = mean_ave_len*length(traj_s); % = average traj length * num trajectories
-    %num_singles = sum_total_particles - totpuncta_intraj;
+    num_singles = sum_total_particles - totpuncta_intraj;
     
-    for n = 1:length(traj_idx)
+    for n = 1:length(puncta) %length puncta may be shorter than traj_indx because some puncta lie aoutside day range
         ltime_cumhist(n) = length(puncta(n).framesobs); % get lifetime
     end
     %ltime_cumhist(ltime_cumhist > max(days_toanalyze)) = 4; %force liftimes into 4-day format
@@ -107,10 +103,11 @@ for k = 1:roi %for each text file of maxima
     allpuncta(k).cumhist = cumhist;
     allpuncta(k).lifetimes = ltime_cumhist;
     allpuncta(k).puncta_matrix = pun;
+    allpuncta(k).numsingles = num_singles;
     
     roi_valid(k) = k;
     roi_valid(roi_valid==0) = [];
-    clear puncta cumhist lentraj ltime_cumhist frame parens traj_idx puncta_interest interest_indx pun
+    clear dataarray puncta cumhist lentraj ltime_cumhist frame parens traj_idx allpuncta_intraj pun
 end
 %
 % all_lifetimes = [];
