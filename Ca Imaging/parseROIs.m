e3b9dcb5ccaf97b0dae420463e6bf66246d28c20
Matt Valley <valley@gmail.com>
2015-01-18 14:59:00 +0100

    add plotting f0 and mean dff

--- Ca Imaging/parseROIs.m
+++ Ca Imaging/parseROIs.m
@@ -67,20 +67,53 @@ for x = 1:length(ALLBLOCKS) %for each block
     %%%%%%%%%%%%%%% do df/f using bottom fraction of data defined by takeforf0
     for r = 1:size(alldat,3) %for each roi
         dims = size(dat);
-        for t = 1:dims(1) %for each trial                
-            smth_trials(t,:) = medfilt1(alldat(t,:,r)) %smooth individual trials before averaging
-            alltrial_base = nanmean(smth_trials, 1); % get mean of all roi trials to use as a baseline
+    % pre-smoothing before baseline subtraction, which is necessary in low s/n
+    % data, is very hard here because z-correction has created many NaNs
+    % producing holes in the waveforms.  To solve this must fundamentally
+    % change how z-correct editing is done which may be a very large
+    % re-write.
+    %     for t = 1:dims(1); %for each trial                
+    %         smth_trials(t,:) = medfilt1(dat(t,:,r),10); %smooth individual trials before averaging
+    %         alltrial_base = nanmean(smth_trials(t,:), 1); % get mean of all roi trials to use as a baseline
+    %     end
         for t = 1:dims(1) %for each trial       
             ft = squeeze(dat(t,:,r)); %timeseries
-            ft = ft - alltrial_base; % subtract alltrial baseline 
+            %ft = ft - alltrial_base; % subtract alltrial baseline 
             [ft_rank, ft_rank_idx] = sort(ft);
-            toget = ciel(length(ft)*takeforf0); %number of frames representing bottom fraction of data values
-            f0 = nanmean(ft_rank(1:toget));
-            df = (ft-f0)/f0; %df/f
+            toget = ceil(length(ft)*takeforf0); %number of frames representing bottom fraction of data values
+            f0(t,r) = nanmean(ft_rank(1:toget)); % get mean of lower percentile of frames
+            df = (ft-f0(t,r))/f0(t,r); %df/f
             alldff(t,:,r) = df;
+            ALLBLOCKS(x).dffmean = nanmean(df); % save mean dff for use later
+            alldffmean(t,r) = nanmean(df);
+            [dff_takeforf0, dff_takeforf0_idx] = sort(df); % recalculate mean of lower percentile of frames using dff
+            dff_percentile(t,r) = nanmean(dff_takeforf0(1:toget));
         end
     end
     
+    [dffmean_rank, dffmean_rankidx] = sort(alldffmean(:));
+        findnan = find(isnan(dffmean_rank) == 1, 1); % get last trace before the sorted nan traces
+    [rankidx_i, rankidx_j] = ind2sub(size(alldffmean), dffmean_rankidx);
+    dff_f0_rank = dff_percentile(:);
+    dff_f0_rank = dff_f0_rank(dffmean_rankidx);
+
+    toplotmin = nanmin(alldff(rankidx_i(1),:,rankidx_j(1)));
+    toplotmax = nanmax(alldff(rankidx_i(1),:,rankidx_j(1)));
+    %toplotmax = nanmax(alldff(rankidx_i(findnan-1),:,rankidx_j(findnan-1)));
+
+    figure;
+        subplot(2,2,[1,2]);
+            plot(dffmean_rank, 'k');hold on;
+            scatter(1:length(dff_f0_rank), dff_f0_rank, '.', 'r');
+        subplot(2,2,3); %max mean dff
+            plot(alldff(rankidx_i(1),:,rankidx_j(1)), 'k'); hold on;
+            plot([1:dims(2)],(dff_percentile(rankidx_i(1),rankidx_j(1))), 'r');
+            ylim([toplotmin, toplotmax]);
+        subplot(2,2,4); %min mean dff
+            plot(alldff(rankidx_i(findnan-1),:,rankidx_j(findnan-1)), 'k'); hold on;
+            plot([1:dims(2)],(dff_percentile(rankidx_i(findnan-1),rankidx_j(findnan-1))), 'r');
+            ylim([toplotmin, toplotmax]);
+        
      %%%%%%%%%%%%%%% do df/f using mean of all timeseries
 %    for r = 1:size(alldat,2) %for each roi
 %         dims = size(dat);          
